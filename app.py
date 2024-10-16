1b1331ec15337f07d8894f1d9941015db38b725e
Bastian-Fuji <yutaro2001.0917@icloud.com>
2024-10-17 01:16:28 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, request, render_template, redirect, url_for
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+# libフォルダをモジュール検索パスに追加
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+
+from flask import Flask, request, render_template, redirect, url_for, send_from_directory, flash
 import lib.extract_ingredients as extract_ingredients
 import lib.get_recipes as get_recipes
-import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
+app.secret_key = 'your_secret_key'  # フラッシュメッセージのためにシークレットキーを設定
 
 # ファイルの保存フォルダを作成
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
 def index():
@@ -20,28 +32,29 @@ def upload_file():
     file = request.files['file']
     if file.filename == '':
         return redirect(request.url)
-    if file:
+    if file and allowed_file(file.filename):
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(file_path)
         return redirect(url_for('show_recipes', image_path=file_path))
-    return redirect('/')
+    else:
+        flash('許可されていないファイル形式です。')
+        return redirect(url_for('index'))
 
 @app.route('/recipes')
 def show_recipes():
     try:
         image_path = request.args.get('image_path')
-
-        # ingredients_list = extract_ingredients.get_ingredients_list(image_path) # 画像から材料リストを抽出
+        ingredients_list = extract_ingredients.get_ingredients_list(image_path) # 画像から材料リストを抽出
         if os.path.exists(image_path):
             os.remove(image_path)
-        # print(ingredients_list)
-        ingredients_list = ['トマト', '白菜'] # テスト用
-
         recipes = get_recipes.fetch_recipes(ingredients_list) # 材料リストを使用してレシピを取得
-
         return render_template('recipes.html', recipes=recipes)
     except Exception as e:
         return str(e)
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
